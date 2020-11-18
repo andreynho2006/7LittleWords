60641fdee4da2d160e327d94a01e6a4684bf501f
Andrei Cirlan <andreynho2006@gmail.com>
2020-11-18 18:55:02 +0200

    Initial Commit

---
+++ 7LittleWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7LittleWords
+//
+//  Created by Andrei Cirlan on 18.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
